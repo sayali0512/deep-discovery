a3f28cf2e76cbd47069c05f14b843e20220da98a
“sayali0512” <skumbhar@italentdigital.com>
2020-08-04 16:33:21 +0530

    updated code

--- lamp-stack-deep-discovery.sh
+++ lamp-stack-deep-discovery.sh
@@ -2,6 +2,7 @@
 
 # get the info if the moodle instance is present, second find the moodle folder path and third find the moodledata path
 get_running_moodle_instance(){
+path=/var/www/html/$service
 service=moodle
 
 if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
@@ -11,5 +12,12 @@ else
 echo "$service is not running!!!"
 
 fi
+if (( $(ps -ef | grep -v grep | locate $path | wc -l) > 0 ))
+then
+echo "$path is present !!!"
+else
+echo "$path is absent  !!!"
+fi
+
 }
 get_running_moodle_instance
